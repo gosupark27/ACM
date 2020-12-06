791ebda567df0da2e1d26cdec8e9f2e8366e359b
gosupark <hpark41@wgu.edu>
2020-12-05 20:55:06 -0700

    Created Business Layer and a unit test for the BL

---
+++ ACM.BLTest/CustomerTest.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using ACM.BL;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ACM.BLTest
+{
+	[TestClass]
+	public class CustomerTest
+	{
+		[TestMethod]
+		public void FullNameTestValid()
+		{
+			//Arrange
+			Customer customer = new Customer
+			{
+				FirstName = "John",
+				LastName = "Doe"
+			};
+
+			string expected = "Doe, John";
+
+			//Act
+			string actual = customer.FullName;
+
+			//Asert
+			Assert.AreEqual(expected, actual);
+
+		}
+
+		[TestMethod]
+		public void FullNameFirstEmpty()
+		{
+			//Arrange
+			Customer customer = new Customer
+			{
+				LastName = "Doe"
+			};
+
+			string expected = "Doe";
+
+			//Act
+			string actual = customer.FullName;
+
+			//Asert
+			Assert.AreEqual(expected, actual);
+
+		}
+
+		[TestMethod]
+		public void FullNameLastEmpty()
+		{
+			//Arrange
+			Customer customer = new Customer
+			{
+				FirstName = "John"
+			};
+
+			string expected = "John";
+
+			//Act
+			string actual = customer.FullName;
+
+			//Asert
+			Assert.AreEqual(expected, actual);
+
+		}
+	}
+}
---
+++ ACM.BLTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("ACM.BLTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ACM.BLTest")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("e519ded3-71c1-4e58-aac4-355b25e786f0")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
